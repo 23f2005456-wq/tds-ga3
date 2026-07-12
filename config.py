f03b01664932fd87b04e7be874daf703f622b431
23f2005456-wq <23f2005456@ds.study.iitm.ac.in>
2026-07-12 21:02:49 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+EMAIL = "23f2005456@ds.study.iitm.ac.in"
+AIPIPE_TOKEN = os.getenv("eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjIwMDU0NTZAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4MDgxMDU5OSwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4MTQxNTM5OX0.ITsuOJ8rtgr8oR0Sd5H8Vm-Czl6x-5wchfFtDbl3I9k", "")
+
+AIPIPE_BASE = "https://aipipe.org/openai/v1"
+TEXT_MODEL = "gpt-4o-mini"
+VISION_MODEL = "gpt-4o"
+EMBED_MODEL = "text-embedding-3-small"
